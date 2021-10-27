002076b839a97f00bdbd247785a4f092bd7dac0f
Hayk10002 <hayk.g.khachatryan@gmail.com>
2021-10-28 00:02:16 +0400

    added math operators and all methods for basic matrix math to class Matrix

--- Matrices/Matrix.hpp
+++ Matrices/Matrix.hpp
@@ -1,49 +1,115 @@
 #pragma once
-
+#include <string>
 
 
 template <size_t my_R, size_t my_C, class my_T = double>
 class Matrix
 {
 private:
+
     my_T my_values[my_R][my_C];
+
+
 
 	template <class = enable_if_t<my_R == my_C>>
 	static Matrix<my_R, my_C, my_T> get_Identity();
 
+
+	template<class o_T>
+	void init(std::initializer_list<std::initializer_list<o_T>> l, my_T def_value = my_T(0))
+	{
+		fill(def_value);
+		for (size_t i = 0; i < my_R; i++)
+		{
+			if (i >= l.size()) break;
+			for (size_t j = 0; j < my_C; j++)
+			{
+				if (j >= (*(l.begin() + i)).size()) break;
+				my_values[i][j] = *((*(l.begin() + i)).begin() + j);
+			}
+		}
+	}
+
+	template<size_t o_R, size_t o_C, class o_T>
+	void init(Matrix<o_R, o_C, o_T> o_m, my_T def_value = my_T(0))
+	{
+		fill(def_value);
+		for (size_t i = 0; i < my_R; i++)
+		{
+			if (i >= o_R) break;
+			for (size_t j = 0; j < my_C; j++)
+			{
+				if (j >= o_C) break;
+				my_values[i][j] = o_m(i, j);
+			}
+		}
+	}
+
 public:
+
+	void fill(my_T fill_value)
+	{
+		for (size_t i = 0; i < my_R; i++) for (size_t j = 0; j < my_C; j++) my_values[i][j] = fill_value;
+	}
+
+
 
     Matrix(my_T fill_value = my_T(0))
     {
-        for(size_t i = 0; i < my_R; i++) for(size_t j = 0; j < my_C; j++) my_values[i][j] = fill_value;
+		fill(fill_value);
     }
 
-    Matrix(std::initializer_list<std::initializer_list<my_T>> l, my_T def_value = my_T(0)) : Matrix(def_value)
+	template<class o_T>
+    Matrix(std::initializer_list<std::initializer_list<o_T>> l, my_T def_value = my_T(0))
     {
-        for(size_t i = 0; i < my_R; i++) 
-        {
-            if(i >= l.size()) break;
-            for(size_t j = 0; j < my_C; j++) 
-            {
-                if(j >= (*(l.begin() + i)).size()) break; 
-                my_values[i][j] = *((*(l.begin() + i)).begin() + j);
-            }
-        }
+		init(l, def_value);
     }
 
     template<size_t o_R, size_t o_C, class o_T>
-    Matrix(Matrix<o_R, o_C, o_T> o_m, my_T def_value = my_T(0)) : Matrix(def_value)
+    Matrix(Matrix<o_R, o_C, o_T> o_m, my_T def_value = my_T(0))
     {
-        for(size_t i = 0; i < my_R; i++) 
-        {
-            if(i >= o_R) break;
-            for(size_t j = 0; j < my_C; j++) 
-            {
-                if(j >= o_C) break; 
-                my_values[i][j] = o_m(i, j);
-            }
-        }
+		init(o_m, def_value);
     }
+
+
+
+	template<class o_T = my_T>
+	void add(o_T value)
+	{
+		for (size_t i = 0; i < my_R; i++) for (size_t j = 0; j < my_C; j++) my_values[i][j] += value;
+	}
+	
+	template<class o_T = my_T>
+	void add(Matrix<my_R, my_C, o_T> matrix)
+	{
+		for (size_t i = 0; i < my_R; i++) for (size_t j = 0; j < my_C; j++) my_values[i][j] += matrix(i, j);
+	}
+
+	template<class o_T = my_T>
+	void substract(o_T value)
+	{
+		for (size_t i = 0; i < my_R; i++) for (size_t j = 0; j < my_C; j++) my_values[i][j] -= value;
+	}
+
+	template<class o_T = my_T>
+	void substract(Matrix<my_R, my_C, o_T> matrix)
+	{
+		for (size_t i = 0; i < my_R; i++) for (size_t j = 0; j < my_C; j++) my_values[i][j] -= matrix(i, j);
+	}
+
+	template<class o_T = my_T>
+	void multiply(o_T value)
+	{
+		for (size_t i = 0; i < my_R; i++) for (size_t j = 0; j < my_C; j++) my_values[i][j] *= value;
+	}
+
+	template<class o_T = my_T>
+	void divide(o_T value)
+	{
+		for (size_t i = 0; i < my_R; i++) for (size_t j = 0; j < my_C; j++) my_values[i][j] /= value;
+	}
+
+
 
     my_T& operator()(size_t r, size_t c)
     {
@@ -52,18 +118,140 @@ public:
         return my_values[r][c];
     }
 
-    void print()
+	Matrix<my_R, my_C, my_T> operator-()
+	{
+		return Zero - (*this);
+	}
+
+	Matrix<my_R, my_C, my_T> operator+()
+	{
+		return (*this);
+	}
+
+	template<class oT>
+	void operator+=(oT val)
+	{
+		add(val);
+	}
+
+	template<class oT>
+	void operator+=(Matrix<my_R, my_C, oT> mat)
+	{
+		add(mat);
+	}
+
+	template<class oT>
+	void operator-=(oT val)
+	{
+		substract(val);
+	}
+
+	template<class oT>
+	void operator-=(Matrix<my_R, my_C, oT> mat)
+	{
+		substract(mat);
+	}
+
+	template<class oT>
+	void operator*=(oT val)
+	{
+		multiply(val);
+	}
+
+	template<class oT>
+	void operator/=(oT val)
+	{
+		divide(val);
+	}
+
+	template<size_t o_R, size_t o_C, class o_T>
+	Matrix<my_R, my_C, my_T>& operator=(Matrix<o_R, o_C, o_T> o_m)
+	{
+		init(o_m);
+		return (*this);
+	}
+
+	template<class o_T>
+	Matrix<my_R, my_C, my_T>& operator=(std::initializer_list<std::initializer_list<o_T>> l)
+	{
+		init(l);
+	}
+
+
+
+    void print(std::string horizontal_sep = " ", std::string vertical_sep = "\n")
     {
         for(size_t i = 0; i < my_R; i++) 
         {
-            for(size_t j = 0; j < my_C; j++) std::cout << my_values[i][j] << ' ';
-            std::cout << std::endl;
+            for(size_t j = 0; j < my_C; j++) std::cout << my_values[i][j] << ((j == my_C - 1) ? "" : horizontal_sep);
+            std::cout << ((i == my_R - 1) ? "" : vertical_sep);
         }
     }
 
+
+
 	static Matrix<my_R, my_C, my_T> Zero;
+
+	Matrix<my_C, my_R, my_T> get_Transpose()
+	{
+		Matrix<my_C, my_R, my_T> res;
+		for (int j = 0; j < my_C; j++) for (int i = 0; i < my_R; i++) res(j, i) = my_values[i][j];
+		return res;
+	}
+
 	template <class = enable_if_t<my_R == my_C>>
 	static Matrix<my_R, my_C, my_T> Identity;
+
+	template <class = enable_if_t<my_R != 1 && my_C != 1>>
+	Matrix<my_R - 1, my_C - 1, my_T> get_Submatrix(size_t delete_r_ind, size_t delete_c_ind)
+	{
+		delete_r_ind = (delete_r_ind > my_R - 1) ? (my_R - 1) : delete_r_ind;
+		delete_c_ind = (delete_c_ind > my_C - 1) ? (my_C - 1) : delete_c_ind;
+		Matrix<my_R - 1, my_C - 1, my_T> res;
+		for (int i = 0, ri = 0; i < my_R; i++)
+		{
+			if (i == delete_r_ind) continue;
+			for (int j = 0, rj = 0; j < my_C; j++)
+			{
+				if (j == delete_c_ind) continue;
+				res(ri, rj) = my_values[i][j];
+				rj++;
+			}
+			ri++;
+		}
+		return res;
+	}
+
+	template <class = enable_if_t<my_R == my_C>>
+	my_T get_Determinant()
+	{
+		if constexpr (my_R == 1) return my_values[0][0];
+		else
+		{
+			my_T res(0);
+			bool sign = 1;
+			for (int i = 0; i < my_R; i++)
+			{
+				my_T det = get_Submatrix(0, i).get_Determinant();
+				res += (sign - !sign) * det * my_values[0][i];
+				sign = !sign;
+			}
+			return res;
+		}
+	}
+
+	template <class = enable_if_t<my_R == my_C>>
+	Matrix<my_R, my_C, my_T> get_Inverse()
+	{
+		my_T det = get_Determinant();
+		if (det == 0) return Identity<>;
+		Matrix<my_R, my_C, my_T> res;
+		for (int i = 0; i < my_R; i++) for (int j = 0; j < my_R; j++) res(i, j) = get_Submatrix(i, j).get_Determinant();
+		for (int i = 0; i < my_R; i++) for (int j = 0; j < my_R; j++) if((i + j) % 2 == 1)res(i, j) = -res(i, j);
+		res = res.get_Transpose();
+		res /= det;
+		return res;
+	}
 };
 
 template <size_t my_R, size_t my_C, class my_T>
@@ -85,3 +273,111 @@ template <class>
 Matrix<my_R, my_C, my_T> Matrix<my_R, my_C, my_T>::Identity = Matrix<my_R, my_C, my_T>::get_Identity();
 
 #define IDENTITY(S, T) Matrix<(S), (S), T>::Identity<>
+
+
+
+template<size_t R, size_t C, class T, class oT>
+Matrix<R, C, T> operator+(Matrix<R, C, T> mat, oT val)
+{
+	mat.add(val);
+	return mat;
+}
+
+template<size_t R, size_t C, class T, class oT>
+Matrix<R, C, T> operator+(oT val, Matrix<R, C, T> mat)
+{
+	mat.add(val);
+	return mat;
+}
+
+template<size_t R, size_t C, class T, class oT>
+Matrix<R, C, T> operator+(Matrix<R, C, T> mat1, Matrix<R, C, oT> mat2)
+{
+	mat1.substract(mat2);
+	return mat1;
+}
+
+
+
+template<size_t R, size_t C, class T, class oT>
+Matrix<R, C, T> operator-(Matrix<R, C, T> mat, oT val)
+{
+	mat.substract(val);
+	return mat;
+}
+
+template<size_t R, size_t C, class T, class oT>
+Matrix<R, C, T> operator-(oT val, Matrix<R, C, T> mat)
+{
+	Matrix<R, C, T> res(-mat);
+	res.add(val);
+	return res;
+}
+
+template<size_t R, size_t C, class T, class oT>
+Matrix<R, C, T> operator-(Matrix<R, C, T> mat1, Matrix<R, C, oT> mat2)
+{
+	mat1.substract(mat2);
+	return mat1;
+}
+
+
+
+template<size_t R, size_t C, class T, class oT>
+Matrix<R, C, T> operator*(Matrix<R, C, T> mat, oT val)
+{
+	mat.multiply(val);
+	return mat;
+}
+
+template<size_t R, size_t C, class T, class oT>
+Matrix<R, C, T> operator*(oT val, Matrix<R, C, T> mat)
+{
+	mat.multiply(val);
+	return mat;
+}
+
+template<size_t R, size_t C, size_t oC, class T, class oT>
+Matrix<R, oC, T> operator*(Matrix<R, C, T> mat1, Matrix<C, oC, oT> mat2)
+{
+	Matrix<R, oC, T> res;
+	for (int i = 0; i < R; i++) for (int j = 0; j < oC; j++)
+	{
+		T sum(0);
+		for (int k = 0; k < C; k++) sum += mat1(i, k) * mat2(k, j);
+		res(i, j) = sum;
+	} 
+	return res;
+}
+
+
+
+template<size_t R, size_t C, class T, class oT>
+Matrix<R, C, T> operator/(Matrix<R, C, T> mat, oT val)
+{
+	mat.divide(val);
+	return mat;
+}
+
+
+
+template<size_t R, size_t C, class T, size_t oR, size_t oC, class oT>
+bool operator==(Matrix<R, C, T> mat1, Matrix<oR, oC, oT> mat2)
+{
+	return 0;
+}
+
+template<size_t R, size_t C, class T, class oT>
+bool operator==(Matrix<R, C, T> mat1, Matrix<R, C, oT> mat2)
+{
+	for (int i = 0; i < R; i++) for (int j = 0; j < C; j++) if (mat1(i, j) != mat2(i, j)) return 0;
+	return 1;
+}
+
+
+
+template<size_t R, size_t C, class T, size_t oR, size_t oC, class oT>
+bool operator!=(Matrix<R, C, T> mat1, Matrix<oR, oC, oT> mat2)
+{
+	return !(mat1 == mat2);
+}
--- Matrices/main.cpp
+++ Matrices/main.cpp
@@ -2,18 +2,49 @@
 #include "Matrix.hpp"
 using namespace std; 
 
+#define PRINT(bt, text, at, eq, ae) cout << bt##text##at; (eq); cout << ##ae
+#define PRINTM(text, eq) PRINT("\n\n", text, "\n\n", (eq).print("\t", "\n\n"), "\n\n")
+#define PRINTO(text, eq) PRINT("\n\n", text, "", cout << (eq), "\n\n")
+
 int main()
 {
-    Matrix<4, 2, int> a(
-        {
-            {1, 2, 3},
-            {4, 5},
-            {6, 7, 8, 9}
-        }
-    );
-    Matrix<3, 3, double> b(a);
-    b.print();
-	IDENTITY(3, double);
+    Matrix<3, 3> a(
+		{
+			{1, 3, 4},
+			{-1, -5, 6},
+			{5, 2, 3}
+		}
+	);
+	Matrix<3, 3> b(
+		{
+			{2, 0, 0},
+			{1, 0, -4}, 
+			{0, 0, 2}
+		}
+	);
+
+	PRINTM("A = ", a);
+	PRINTM("B = ", b);
+	PRINTM("A + 5 = ", a + 5);
+	PRINTM("A + B = ", a + b);
+	PRINTM("A - 5 = ", a - 5);
+	PRINTM("5 - A = ", 5 - a);
+	PRINTM("A - B = ", a - b);
+	PRINTM("A * 5 = ", a * 5);
+	PRINTM("A * B = ", a * b);
+	PRINTM("A / 5 = ", a / 5);
+	PRINTO("Element in first row and second column of matrix A (or A(0, 1) ) is ", a(0, 1));
+	PRINTM("Transpose matrix of A is ", a.get_Transpose());
+	PRINTM("Submatrix of A without first row and second column is ", a.get_Submatrix(0, 1));
+	PRINTO("Determinant of A is ", a.get_Determinant());
+	PRINTO("Determinant of B is ", b.get_Determinant());
+	cout << "\n\nIf you try to calculate determinant for non-square matrix it will be a compile error\n\n";
+	PRINTM("Inverse matrix of A is ", a.get_Inverse());
+	cout << "\n\nIf you try to calculate inverse matrix of non-square matrix it will be a compile error and if you try to calculate inverse matrix of matrix which has determinant equal to 0, function will return Identity matrix of size of your matrix \n\n";
+	PRINTM("Inverse matrix of B can't be calculated => function returns Idenitiy matrix ", b.get_Inverse());
+
+
+    
 	system("pause");
     return 0;
 }
